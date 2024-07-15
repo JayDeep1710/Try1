1b2b6c3c6e2d4d370ad11f70111a2d1b99790fd2
JayDeep1710 <JAYDEEPSINGH172001@GMAIL.COM>
2024-07-15 21:52:56 +0530

    el

--- app2.py
+++ app2.py
@@ -27,7 +27,8 @@ if prompt :=st.chat_input("whats up?"):
         response1 = "Since your jeans have a classic black color and a flattering flared cut, you've got a lot of options for tops! Here are a few ideas depending on the look you're going for: \n\n •Solid colored t-shirt: A crisp white tee or a black crewneck tee are always failsafe options. You can also go for a grey tee or a jewel tone like emerald green or sapphire blue.\n\n•Graphic t-shirt:  A graphic tee with a cool print or a band tee can add a touch of personality to your outfit.\n\n•Striped t-shirt:  A classic striped tee in black and white or navy and white is a laid-back yet polished choice. \n\nYou can choose one of these depending on the requirement."
     if prompt == "Which one will be the best for a casual outing?":
         response1 = "Out of the options listed, for a casual, a  solid colored t-shirt or a graphic t-shirt would be the best choices. Here's why: \n\n •Lightweight and breathable: A t-shirt made from cotton or linen will keep you cool and comfortable in the warm weather.\n\n •Relaxed style: A t-shirt offers a casual and laid-back vibe, perfect for a relaxed outing.\n\n •Versatility: You can easily dress it up or down with accessories like a statement necklace, a scarf, or a denim jacket."
-
+    else:
+        response1 = "Server is down right now"
     response = f"AI : {response1}"
     
     
